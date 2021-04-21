4c6cf433a91c55b61195a8ba09dec2849bce02e6
Facundo Giuliani <facundo.giuliani@gmail.com>
2021-04-20 23:58:42 -0300

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+// The Storyblok Client
+import Storyblok from "../lib/storyblok";
+import useStoryblok from "../lib/storyblok-hook";
+import DynamicComponent from "../components/DynamicComponent";
+
+export default function Home(props) {
+  // the Storyblok hook to enable live updates
+  const story = useStoryblok(props.story);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,57 +17,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <header>
+        <h1>{story ? story.name : "My Site"}</h1>
+      </header>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        {story
+          ? story.content.body.map((blok) => (
+              <DynamicComponent blok={blok} key={blok._uid} />
+            ))
+          : null}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  let slug = "home";
+  let params = {
+    version: "draft", // or 'published'
+  };
+
+  if (context.preview) {
+    params.version = "draft";
+    params.cv = Date.now();
+  }
+
+  let { data } = await Storyblok.get(`cdn/stories/${slug}`, params);
+
+  return {
+    props: {
+      story: data ? data.story : false,
+      preview: context.preview || false,
+    },
+    revalidate: 10,
+  };
 }
